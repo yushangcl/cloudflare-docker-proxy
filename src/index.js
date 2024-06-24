50200611cdc9fec37c8d2e75165d5c1a43a989e9
Henry <yushangcl@users.noreply.github.com>
2024-06-24 15:49:10 +0800

    Update index.js
    init

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const routes = {
 
   // staging
   // "docker-staging.libcuda.so": dockerHub,
-  "cloudflare-docker-proxy.gdd.workers.dev/": dockerHub,
+  "cloudflare-docker-proxy.gdd.workers.dev/": "https://registry-1.docker.io",
 };
 
 function routeByHosts(host) {
